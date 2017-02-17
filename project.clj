be372a774a8324c50ba5122df7acff073b969ed5
Nikos Fertakis <nikos.fertakis@gmail.com>
2017-02-17 22:22:01 +0200

    Refactor code and add cli options

--- project.clj
+++ project.clj
@@ -1,12 +1,13 @@
 (defproject clj-code-poster "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Code Poster Generator in Clojure"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [net.mikera/imagez "0.12.0"]
                  [dali "0.7.3"]
-                 [com.taoensso/timbre "4.8.0"]]
+                 [com.taoensso/timbre "4.8.0"]
+                 [org.clojure/tools.cli "0.3.5"]]
   :main ^:skip-aot clj-code-poster.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/clj_code_poster/core.clj
+++ src/clj_code_poster/core.clj
@@ -1,80 +1,141 @@
 (ns clj-code-poster.core
-  (:require clojure.string
+  (:require [clojure.string :as string]
+            [clojure.tools.cli :as cli]
             [mikera.image.core :as image]
             [mikera.image.colours :as colours]
             [taoensso.timbre :as timbre]
             [dali.io :as io])
   (:gen-class))
 
-(def image-file "clj.png")
+(set! *warn-on-reflection* true)
+(set! *unchecked-math* true)
 
-(def code-file "resources/core.clj")
+;; Image functions
 
-(def img
-  (image/load-image-resource image-file))
-(def width (.getWidth img))
-(def height (.getHeight img))
+(defn- components-hex
+  "Get hex colour values from image pixels."
+  [^long rgb]
+  (colours/with-components [[r g b] rgb]
+    (->> (map #(format "%02x" %) [r g b])
+         (apply str "#"))))
 
-(def ratio 0.6)
+(defn- img->pixel-rows
+  "Convert a BufferedImage to a 2D pixel array of rows."
+  [^java.awt.image.BufferedImage image]
+  (let [width (.getWidth image)]
+    (partition width (image/get-pixels image))))
 
-(defn components-hex
-  "Get hex colour values from image pixels"
-  ([^long rgb]
-   (apply str "#"
-          (map #(format "%02x" %)
-               [(colours/extract-red rgb)
-                (colours/extract-green rgb)
-                (colours/extract-blue rgb)]))))
+;; Code functions
 
-;; TODO: We should convert to hex only immediately before creating the SVG
-;; structure, i.e. in TextField->svg
-(def img-hex
-  (->>
-   (map components-hex (image/get-pixels img))
-   (partition width)))
+(def ^:private font-ratio
+  "Ratio of the fonts we used, needed for various transformations.
+  TODO: This should be a map of font-name -> ratio."
+  0.6)
 
-(defn join-code [code]
-  (-> (clojure.string/trim code)
-      (clojure.string/replace #"\s*\n+\s*" " ")
-      (clojure.string/replace #"\s" " ")))
+(defn- join-code
+  "Cleanup source code by removing spaces, newlines etc."
+  [code]
+  (-> (string/trim code)
+      (string/replace #"\s*\n+\s*" " ")
+      (string/replace #"\s" " ")))
 
-;; TODO: Pass it as a lazy, cycling sequence.
-(def code
-  (apply str (repeat 100 (-> (slurp code-file)
-                            join-code
-                            ;;      cycle
-                            ))))
-
-(defn word [x y]
-  (get code (+ (* width y) x)))
+;; Data modeling
 
 (defrecord TextField [text x y fill])
 
 (defn TextField->svg [{:keys [text x y fill]}]
-  [:text {:x x :y y :fill fill} text])
+  [:text {:x (* font-ratio x) :y y :fill (components-hex fill)} text])
 
-(defn produce-row [row-pixels y]
+;; Main algorithm
+
+(defn- add-character-to-row
+  "Adds a character `c` to a `row` (vector) of TextFields. Each TextField has
+  a colour fill, a chunk of text and `x` and `y` coordinates. Each character
+  has the `colour` of its matching pixel (based on location). The possible cases
+  are:
+  1. The `row` is empty: we create a new TextField containing the character and
+     filled with the corresponding `colour`, placed on the pixel's `x` and `y`
+     coordinates..
+  2. The row is not empty and the last TextField is of the same fill as the
+     character's `colour`: we simply append the character to the TextField.
+  3. The row is not empty but the last TextField is of different fill than the
+     character's `colour`: we create a new TextField, same as (1)."
+  [c row colour x y]
+  (if-let [{:keys [fill] :as tf} (peek row)]
+    (if (= fill colour)
+      (conj (pop row) (update tf :text #(str % c)))
+      (conj row (->TextField c x y colour)))
+    (conj row (->TextField c x y colour))))
+
+(defn- produce-row
+  "Accepts a vector of pixels, a vector of text and a row index. It creates
+  a vector of [pixel, character, index] tuples and reduces them to a row of
+  TextFields."
+  [pixel-row code-row y]
   (reduce
-   (fn [coll [p idx]]
-     (let [c (word idx y)]
-       (if-let [{:keys [fill] :as tf} (peek coll)]
-         (if (= fill p)
-           (conj (pop coll) (update tf :text #(str % c)))
-           (conj coll (->TextField c (* ratio idx) y p)))
-         (conj coll (->TextField c (* ratio idx) y p)))))
-   [] row-pixels))
+   (fn [coll [p c x]]
+     (add-character-to-row c coll p x y))
+   [] (map vector pixel-row code-row (range))))
 
-(def document
-  [:dali/page {:viewbox "0 0 300 300"
-               :width 2000 :height 2000
-               "xml:space" "preserve"
-               :style "font-family: 'Source Code Pro'; font-size: 1; font-weight: 900;"}
-   (->> (mapcat
-         (fn [[x y]] (produce-row (map vector x (range)) y))
-         (map vector img-hex (range)))
-        (map TextField->svg))])
+(defn- create-text-fields
+  "Accepts a BufferedImage and a chunk of text. It splits the image into rows of
+  pixels, creates a lazy sequence of text chunks of length same as the image
+  width, and combines them to create a sequence of TextFields."
+  [^java.awt.image.BufferedImage image code]
+  (let [width (.getWidth image)
+        pixel-rows (img->pixel-rows image)
+        code-rows (partition width (cycle code))]
+    (mapcat produce-row pixel-rows code-rows (range))))
+
+(defn- create-document [image code]
+  [:dali/page
+   {:viewbox "0 0 300 300"
+    :width 2000 :height 2000
+    "xml:space" "preserve"
+    :style "font-family: 'Source Code Pro'; font-size: 1; font-weight: 900;"}
+   (map TextField->svg (create-text-fields image code))])
+
+(defn produce-code-poster [image-file code-file svg-file]
+  ;; TODO: Apply needed image scaling
+  (let [image (image/load-image image-file)
+        code (-> (slurp code-file) join-code)
+        document (create-document image code)]
+    (io/render-svg document svg-file)))
+
+(def cli-options
+  [["-i" "--image IMAGE" "Image file"]
+   ["-c" "--code CODE" "Code file"]
+   ["-o" "--out OUT" "SVG output file"]
+   ["-h" "--help"]])
+
+(defn usage [options-summary]
+  (->> ["Create beautiful code posters."
+        ""
+        "Usage: clj-code-poster [options] action"
+        ""
+        "Options:"
+        options-summary
+        ""
+        "Please refer to the github page for more information."]
+       (string/join \newline)))
+
+(defn error-msg [errors]
+  (str "The following errors occurred while parsing your command:\n\n"
+       (string/join \newline errors)))
+
+(defn exit [status msg]
+  (println msg)
+  (System/exit status))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Create beautiful code posters!"
   [& args]
-  (println "Hello, World!"))
+  (let [{:keys [options arguments errors summary]}
+        (cli/parse-opts args cli-options)
+        {:keys [image code out]} options]
+    (cond
+      (:help options) (exit 0 (usage summary))
+      (not (and image code out)) (exit 1 (usage summary))
+      errors (exit 1 (error-msg errors)))
+    (timbre/info "Creating poster, please be patient...")
+    (produce-code-poster image code out)))
